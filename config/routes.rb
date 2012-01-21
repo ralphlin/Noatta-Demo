2f0b00a66d076fb4c9ab739fc25f0845d69f2ca2
Ralph Lin <ralph.y.lin@gmail.com>
2012-01-20 16:35:08 -0800

    still trying to get heroku to work, modified routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ SampleApp::Application.routes.draw do
   match '/about', :to => 'pages#about'
   match '/help', :to => 'pages#help'
   match 'signup', :to => 'users#new'
+  match '/home', :to=> 'pages#home'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
