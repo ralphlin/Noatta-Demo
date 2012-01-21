d24ed7bd02128c03e76d21e2fd094ec4babe13ce
Ralph Lin <ralph.y.lin@gmail.com>
2012-01-20 17:02:31 -0800

    heroku fixed with typo in routes.db

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,8 @@ SampleApp::Application.routes.draw do
   match '/contact', :to => 'pages#contact'
   match '/about', :to => 'pages#about'
   match '/help', :to => 'pages#help'
-  match 'signup', :to => 'users#new'
-  match '/home', :to=> 'pages#home'
-
+  match '/signup', :to => 'users#new'
+  
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
