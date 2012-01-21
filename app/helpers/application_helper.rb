231792e019f0b1562d7c8912ec6ff461dee7d1f6
Ralph Lin <ralph.y.lin@gmail.com>
2012-01-20 17:38:24 -0800

    reverted to added application helper (end ch4) to redo to work for heroku

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,2 @@
 module ApplicationHelper
-
-	# return a title on a per-page load
-	def title
-		base_title = "Ruby on Rails Tutorial Sample App"
-		if @title.nil?
-			base_title
-		else
-			"#{base_title} | #{@title}"
-		end
-	end
 end
