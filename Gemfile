e0a41546593125f1b12fbd4263da68c0c128f268
Ralph Lin <ralph.y.lin@gmail.com>
2012-01-23 18:50:59 -0800

    User model with passwords and user show page

--- Gemfile
+++ Gemfile
@@ -7,6 +7,8 @@ gem 'rails', '3.1.1'
 
 gem 'sqlite3'
 
+gem 'gravatar_image_tag'
+
 group :development do
 	gem 'annotate'
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,52 @@
+require 'digest'
 class User < ActiveRecord::Base
-	attr_accessible :name, :email
+	attr_accessor :password
+	attr_accessible :name, :email, :password, :password_confirmation
+
 
 	validates :name, :presence => true, 
 					 :length => { :maximum => 50 }
 	validates :email, :presence => true, 
 					  :uniqueness => { :case_sensitive => false }
-					  
+	validates :password, :presence => true,
+						 :confirmation => true,
+						 :length => { :within => 6..40 }				  
+
+	before_save :encrypt_password
+
+	# Return true if the user's password matches the submitted password
+	def has_password?(submitted_password)
+		# compare encrypted_word with encrypted version of
+		# submitted_password
+		encrypted_password == encrypt(submitted_password)
+	end
+
+	# method to check to see if submitted email/password is
+	# in the database.
+	# returns nil if there is no such user
+	# returns user if email/password matches user in db
+	def self.authenticate(email, submitted_password)
+		user = find_by_email(email)
+		return nil if user.nil?
+		return user if user.has_password?(submitted_password)
+	end
+
+	private
+		def encrypt_password
+			self.salt = make_salt unless has_password?(password)
+			self.encrypted_password = encrypt(password)
+		end
+
+		def encrypt(string)
+			secure_hash("#{salt}--#{string}")
+		end
+
+		def make_salt
+			secure_hash("#{Time.now.utc}--#{password}")
+		end
+
+		def secure_hash(string)
+			Digest::SHA2.hexdigest(string)
+		end
 
 end
